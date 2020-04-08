5d8b6ebe9db70ce7b26070f41135718b64d97702
Rahul <rahulgundiya34@gmail.com>
2020-04-08 19:54:01 +0530

    Outputing-list

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React , {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
+//import person from './Person/Person';
 //import UserInput from './UserInput/UserInput';
 //import UserOutput from './UserOutput/UserOutput';
 
@@ -66,14 +67,14 @@ if(this.state.showPersons)
  persons=( 
 
 <div>
+  {this.state.persons.map(person=>{
+    return <Person name={person.name} age={person.age}  / >
+  
+  
+  }
+  )}
      
-     <Person name= { this.state.persons[0].name } age= {this.state.persons[0].age }  / >
-       
-       
-     <Person name= { this.state.persons[1].name } age= {this.state.persons[1].age } click ={this.switchNameHandler.bind(this , "Max!")} changed ={this.nameChangedHandler} >My Hobbies:Racing </Person>
-     
-     
-     <Person name= { this.state.persons[2].name } age= {this.state.persons[2].age }  />
+ 
 
     </div> 
 
