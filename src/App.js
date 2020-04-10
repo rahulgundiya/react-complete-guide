2e9afc8fa1c2144123999f5cc16c74c92587df19
Rahul <rahulgundiya34@gmail.com>
2020-04-10 13:13:34 +0530

    add styling dynamically

--- src/App.js
+++ src/App.js
@@ -3,62 +3,117 @@
 import React , {Component} from 'react';
 //import logo from './logo.svg';
 import './App.css';
-// import Person from './Person/Person';
+import Person from './Person/Person';
 //import person from './Person/Person';
 //import UserInput from './UserInput/UserInput';
 //import UserOutput from './UserOutput/UserOutput';
-import Validation from "./Validation/Validation";
-import Char from './Char/Char'
+
+
 class App extends Component {
-state ={
-userInput: '' 
+state = {
+  persons:[ {
+   id:"vasb1" , name:"Max" , age:28 } ,
+    { id:"fasd1" , name:"Menu" , age:30} ,
+    {id:"tyhg2" ,name:"Staphanie"  , age:35}
+  ] ,
+
+otherState:'some other value' ,
+showPersons:false
+}
+switchNameHandler=(newName)=>{
+  console.log("Was clicked");
+
+  this.setState({
+    persons: [{
+      name:newName , age:40 } ,
+      {name:"Manoj" , age:25} ,
+      {name:"Staphanie"  , age:35}
+      
+    ]
+   
+  })
 
 }
+nameChangedHandler=(event , id) =>{
+const personIndex = this.state.persons.findIndex(p=>{
+  return p.id  === id;
+}) 
 
-inputChangedHandler=(event)=>
+const person = {
+  ...this.state.persons[personIndex]
+  
+}
+
+person.name = event.target.value;
+const persons = [...this.state.persons] ;
+
+persons[personIndex]=person;
+
+
+
+  this.setState({
+    persons: persons })
+}
+
+deletePersonHandler(personIndex) 
 {
-this.setState({userInput:event.target.value})
-}
+//const persons =this.state.persons;
+const persons = [...this.state.persons];
+persons.splice(personIndex ,1);
+this.setState({persons:persons});
 
-deleteCharHandler=(index)=>{
- const text  = this.state.userInput.split('');
- text.splice( index ,1);
- const updatedText = text.join('');
- this.setState({userInput:updatedText});
+}
+togglePersonsHandler=()=>{
+  const doesShow=this.state.showPersons;
+  this.setState({showPersons:!doesShow})
+
 }
   render()
   {
 
-    const charList =
-      this.state.userInput.split('').map((ch ,index)=>{
-        return <Char  character={ch}  key ={index}
-        clicked ={()=> this.deleteCharHandler(index)}
-        />
-      })
-    
+    const style ={
+      backgroundColor : 'green' ,
+      color:'white' ,
+    font : 'inherit' ,
+  border : ' 1px solid blue' ,
+  padding :'8px' ,
+  cursor :'pointer'
+    }
+let persons =null ;
+if(this.state.showPersons)
+{
+ persons=( 
 
+<div>
+  {this.state.persons.map((person ,index)=>{
+    return <Person name={person.name} age={person.age}
+     click ={()=> this.deletePersonHandler(index)}
+     key ={person.id}
+     changed = {(event)=>this.nameChangedHandler(event ,person.id)}
+    / >
+  
+  
+  }
+  )}
+     
+ 
 
+    </div> 
 
+ )
+ style.backgroundColor= 'red';
+}
 
   return (
 
     <div className="App">
 
-<ol>
-          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
-        <hr />
-        <input type="text" onChange={this.inputChangedHandler} value ={this.state.userInput}  />
-  <p>{this.state.userInput}</p>
-
-  <Validation  inputLength = {this.state.userInput.length}  />
-  {charList}
+      <h1>hi ,I'm react app</h1>
+     <p>This is really Work..!!!</p>
+     <button style ={style} onClick ={this.togglePersonsHandler}>Switch-Name</button>
+       {persons}  
+   
+    
     </div>
     
   
